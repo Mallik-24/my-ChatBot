3a7c9b73f2107f8c0a28cf47504907ed13d50b9b
Mallik-24 <124077143+Mallik-24@users.noreply.github.com>
2023-04-20 08:59:35 +0530

    Add files via upload

---
+++ jenkinsfile.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                git branch: 'main', url: 'https://github.com/VattamBhavaniPrasad5i5/my-ChatBot.git'
+                sh 'pip install rasa'
+                sh 'rasa train'
+                sh 'rasa test'
+            }
+        }
+
+        stage('Dockerize') {
+            steps {
+                sh 'docker build -t my-chatbot .'
+                sh 'docker tag my-chatbot my-docker-repo/my-chatbot:latest'
+                sh 'docker push my-docker-repo/my-chatbot:latest'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'my-creds-id', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                    sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
+                    sh 'kubectl apply -f kubernetes/my-chatbot-deployment.yaml'
+                    sh 'kubectl apply -f kubernetes/my-chatbot-service.yaml'
+                }
+            }
+        }
+    }
+}
+
